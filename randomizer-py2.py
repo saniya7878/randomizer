c8e21194ff6d180aaca0e1b68208fb12df50796d
saniya7878 <69676965+saniya7878@users.noreply.github.com>
2020-08-14 19:22:56 +0530

    Add files via upload
    it is a simple python3 program (can be run on python2 also with some simple changes) which gives 10 random numbers between 0-500 in ascending order. 
    You can run the python file if you have python installed on your system
                                           (or)
    run the .exe file

---
+++ randomizer-py2.py
@@ -0,0 +1,23 @@
+#! usr/bin.env python
+import random
+import os
+
+def print_list(numbers_list):
+    for number in numbers_list:
+        print(number)
+
+
+def randomizer(numbers_required):
+    i = 0
+    numbers = []
+    while i < numbers_required:
+        n = random.randint(1, 500)
+        numbers.append(n)
+        i = i + 1
+    return numbers
+
+
+random_numbers = randomizer(10)
+random_sorted_numbers = sorted(random_numbers)
+print_list(random_sorted_numbers)
+raw_input("[+] Press ENTER to close...>>")
